e8bf628583083f90637ac09899ac378316ba7327
SuchLuukie <62255292+SuchLuukie@users.noreply.github.com>
2022-02-27 23:32:32 +0100

    Started work on scheduler

---
+++ schedule_module.py
@@ -0,0 +1,17 @@
+# Import libraries
+import json
+
+# Class to keep track and execute schedule reminders
+class ScheduleModule:
+    def __init__(self):
+        pass
+
+
+    # Create a schedule
+    def create_schedule(self, name, datetime, repeat: False):
+        return
+
+
+    # Execute schedule reminder
+    def schedule_reminder(self):
+        return
