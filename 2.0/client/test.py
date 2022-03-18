4f926bb9b7dbee44ce24c0903701a4ab67c18214
SuchLuukie <62255292+SuchLuukie@users.noreply.github.com>
2022-03-18 13:27:02 +0100

    intent update endpoint finished

--- 2.0/client/test.py
+++ 2.0/client/test.py
@@ -11,7 +11,7 @@ r = requests.post("http://127.0.0.1:5000/api/login", json=data)
 print(r)
 print(r.json())
 
-command = {"command": "whats the weather like bruv"}
+command = {"intent": "whats the weather like", "command": "weather_forecast"}
 nr = requests.post("http://127.0.0.1:5000/api/intent_update", json=command, headers=r.json())
 print(nr)
 print(nr.json())
